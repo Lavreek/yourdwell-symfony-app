28028c05400121e33ab0ada6a872c36e3871d14f
Михаил Иванов <hobbit45611@gmail.com>
2022-03-09 21:02:20 +0300

    Fix env error

--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -11,7 +11,18 @@
         {
             return [
                 new TwigFunction('placeCards', [$this, 'cardConstruction']),
+                new TwigFunction('jsonCards', [$this, 'jsonCardConstruction']),
             ];
+        }
+
+        public function jsonCardConstruction(string $card)
+        {
+            echo "
+            <div class='col' style='text-align:center;'>
+                <div class='card shadow-sm'>
+                    <p><a class='text-dark nav-link' href='http://hh.yourdwell.ru/".$card."'>".$card."</a></p>
+                </div>
+            </div>";
         }
 
         public function cardConstruction(array $cards)
